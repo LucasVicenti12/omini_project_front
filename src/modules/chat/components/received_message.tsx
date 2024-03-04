faef13dafab6913bfb8140cc5cd23b1ce1b37151
Lucas Vicenti <lucasvicenti60@gmail.com>
2024-03-04 00:15:23 -0300

    adjusted the attach button in received message

--- src/modules/chat/components/received_message.tsx
+++ src/modules/chat/components/received_message.tsx
@@ -11,25 +11,25 @@ export const ReceivedMessage = ({message, time}: ReceivedMessageProps) => {
 
     return (
         <div
-            className={"relative min-h-3 border w-auto p-2 bg-muted rounded-b-2xl rounded-r-2xl"}
+            className={"w-full flex"}
             onMouseOver={() => setShowAttachMessage(true)}
-            onMouseLeave={() => {
-                setTimeout(() => {
-                    setShowAttachMessage(false)
-                }, 2000)
-            }}
+            onMouseLeave={() => setShowAttachMessage(false)}
         >
-            <span className={"text-muted-foreground"}>{message ?? ''}</span>
-            <span className={"text-muted-foreground absolute text-xs -bottom-2 -right-8"}>{time ?? ''}</span>
-            {
-                showAttachMessage && (
-                    <span className={"text-muted-foreground absolute text-xs bottom-4 -right-10"}>
-                        <Button variant={"ghost"} size={"icon"} className={"h-5 w-5"}>
+            <div
+                className={"relative min-h-3 border w-auto p-2 bg-muted rounded-b-2xl rounded-r-2xl"}
+            >
+                <span className={"text-muted-foreground"}>{message ?? ''}</span>
+                <span className={"text-muted-foreground absolute text-xs -bottom-2 -right-8"}>{time ?? ''}</span>
+                {
+                    showAttachMessage && (
+                        <span className={"text-muted-foreground absolute text-xs bottom-4 -right-10"}>
+                        <Button variant={"ghost"} size={"icon"} className={"h-6 w-6"}>
                         <Undo2 size={"16"}/>
                             </Button>
                     </span>
-                )
-            }
+                    )
+                }
+            </div>
         </div>
     );
 }
